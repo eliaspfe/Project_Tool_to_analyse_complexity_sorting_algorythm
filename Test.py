8149040b70cf2b436cdb67b12c6723884ecf1cd7
eliaspfe <elias.pfeiffer@gmx.de>
2024-11-04 15:13:57 +0100

    changed

--- Test.py
+++ Test.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 import os
 from langchain_openai import ChatOpenAI
 
-os.environ["OPENAI_API_KEY"] = "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"
 
 model = ChatOpenAI(model="gpt-3.5-turbo")
 
